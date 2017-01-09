913bc2431f8dc186117ca41e150ffcc58ff0e36d
jackbinysh <jack.binysh@gmail.com>
2017-01-09 21:26:11 +0000

    working on modifiyng the knot curve tracing - this version of the code is an older, simpler one - we will need to merge it with the new feaatures later

--- FN_knot_surface.h
+++ FN_knot_surface.h
@@ -15,13 +15,15 @@
 #include <math.h>
 #include <vector>
 #include <time.h>
+//includes for the signal processing
+#include <gsl/gsl_multimin.h>
+#include <gsl/gsl_vector.h>
 
 using namespace std;
 
 #define FROM_PHI_FILE 0
 #define FROM_SURFACE_FILE 1
 #define FROM_UV_FILE 2
-#define FROM_KNOT_FILE 3
 
 const double sixth = 1.0/6.0;
 
@@ -43,9 +45,6 @@ struct knotpoint
     double ax;       //grad vector x coord
     double ay;       //grad vector y coord
     double az;       //grad vector z coord
-	double vx;       // x velocity
-    double vy;       //y velocity
-    double vz;       //z velocity
     double twist;    //local twist value
     double writhe;   //local writhe value
     double length;   //length of line
@@ -78,36 +77,29 @@ double initialise_knot();
 
 double init_from_surface_file(void);
 
-double init_from_knot_file(void);
-
 /*************************Functions for B and Phi calcs*****************************/
 
-void initial_cond(double *x, double *y, double *z, double *phi, int *missed);
+void initial_cond(double *x, double *y, double *z, double *phi);
 
 void phi_calc(double *x, double *y, double *z, double *phi);
 
-void B_field_calc(double *x, double *y, double *z, double *Bx, double *By, double *Bz, double *Bmag, int *ignore, int *ignore1, int *missed);
-
-void phi_calc_B(double *Bx, double *By, double *Bz, double *Bmag, int *ignore, int *ignore1, int *missed, double *phi);
-
-int pathfind(int i0, int j0, int k0, int ie, int je, int ke, int *pi, int *pj, int *pk, int *ignore, double *Bx, double *By, double *Bz, double *Bmag);
-
 //FitzHugh Nagumo functions
-void uv_initialise(double *phi, double *u, double *v, int* missed);
+void uv_initialise(double *phi, double *u, double *v);
 void crossgrad_calc(double *x, double *y, double *z, double *u, double *v, double *ucvx, double *ucvy, double *ucvz);
-void find_knot_properties(double *x, double *y, double *z, double *ucvx, double *ucvy, double *ucvz, double* u,double t);
+void find_knot_properties(double *x, double *y, double *z, double *ucvx, double *ucvy, double *ucvz, double* u,double t, gsl_multimin_fdfminimizer *minimizerstate);
 void uv_update(double *u, double *v, double *ku, double *kv, double *kut, double *kvt, double *uold, double *vold);
 void uv_add(double *u, double *v, double* uold, double *vold, double *ku, double *kv, double *kut, double *kvt, double inc, double coeff);
 void uv_update_euler(double *u, double *v, double *D2u);
 
-// 3d geometry functions
-int intersect3D_SegmentPlane( knotpoint SegmentStart, knotpoint SegmentEnd, knotpoint PlaneSegmentStart, knotpoint PlaneSegmentEnd, double& IntersectionFraction, std::vector<double>& IntersectionPoint );
+double my_f(const gsl_vector* v, void* params);
+void my_df(const gsl_vector* v, void* params,gsl_vector *df);
+void my_fdf (const gsl_vector *x, void *params, double *f, gsl_vector *df); 
 
 /*************************File reading and writing*****************************/
 
-void print_B_phi(double *x, double *y, double*z, double *phi, int *missed);
+void print_B_phi(double *x, double *y, double*z, double *phi);
 void print_uv(double *x, double *y, double *z, double *u, double *v, double *ucvx, double *ucvy, double *ucvz, double t);
 int phi_file_read(double *phi);
-void print_knot(double *x, double *y, double *z, double t, std::vector<knotpoint>& knotcurve);
+void print_knot(double *x, double *y, double *z, double t);
 int uvfile_read(double *u,double *v);
 void print_info(int Nx, int Ny, int Nz, double dtime, double h, const bool periodic,  int option, string knot_filename, string B_filename);
