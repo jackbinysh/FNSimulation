95614d53840e35590454a20fae9d4af6512c5a71
jackbinysh <jack.binysh@gmail.com>
2017-05-24 16:53:41 +0100

    instead of hardcoded variables here ,put placeholder names that a script can find and replace

--- FN_Constants.h
+++ FN_Constants.h
@@ -18,9 +18,9 @@ FROM_UV_FILE: Skip initialisation, run FN dynamics from uv file
 FROM_FUNCTION: Initialise from some function which can be implemented by the user in phi_calc_manual. eg using theta(x) = artcan(y-y0/x-x0) to give a pole at x0,y0 etc..:wq
  */
 //if ncomp > 1 (no. of components) then component files should be separated to 'XXXXX.txt" "XXXXX2.txt", ....
-const int option = FROM_UV_FILE;         //unknot default option
-std::string knot_filename = "eight13";      //if FROM_SURFACE_FILE assumed input filename format of "XXXXX.stl"
-std::string B_filename = "uv_plot11.vtk";    //filename for phi field or uv field
+const int option = INSERT_INITIALISATION_TYPE;         //unknot default option
+std::string knot_filename = "";      //if FROM_SURFACE_FILE assumed input filename format of "XXXXX.stl"
+std::string B_filename = INSERT_UV_FILENAME;    //filename for phi field or uv field
 
 // OPTION - what kind of boundary condition
 const BoundaryType BoundaryType=ALLPERIODIC;
@@ -29,24 +29,24 @@ const BoundaryType BoundaryType=ALLPERIODIC;
 #define PRESERVE_RATIOS 0  //1 to scale input file preserving the aspect ratio
 
 // OPTION - how long should it run, when do you want data printed, what time value should it start at 
-const double TTime = 3000;       //total time of simulation (simulation units)
-const double UVPrintTime = 11;       //print out UV every # unit of time (simulation units)
+const double TTime = INSERT_RUNTIME;       //total time of simulation (simulation units)
+const double UVPrintTime = 500;       //print out UV every # unit of time (simulation units)
 const double VelocityKnotplotPrintTime = 11.2;       //print out the velocity every # unit of time (simulation units)
 const double FrequentKnotplotPrintTime = 1; // print out the knot , without the velocity
-const double InitialSkipTime = 10;       // amout to skip before beginning the curve tracing
+const double InitialSkipTime = INSERT_SKIPTIME;       // amout to skip before beginning the curve tracing
 
 // OPTION - what grid values do you want/ timestep
 //Grid points
-const double h = 0.6;            //grid spacing
-const int initialNx = 285;   //No. points in x,y and z
-const int initialNy = 285;
-const int initialNz = 285;
+const double h = 0.53;            //grid spacing
+const int initialNx = INSERT_NX;   //No. points in x,y and z
+const int initialNy = INSERT_NY;
+const int initialNz = INSERT_NZ;
 // timestep
 const double dtime = 0.02;         //size of each time step
 
 // OPTION - do you want to resize the box? if so, when?
-const bool BoxResizeFlag = 0;
-const double BoxResizeTime = 400;
+const bool BoxResizeFlag = 1;
+const double BoxResizeTime = 1000;
 
 
 
