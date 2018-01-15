2ca3e17de6d9bdbe7659036eee4779068488c1f2
jackbinysh <jack.binysh@gmail.com>
2018-01-15 17:44:40 +0000

    altering these rarely touched settings to their correct values

--- Simulation/FN_Constants.h
+++ Simulation/FN_Constants.h
@@ -78,9 +78,9 @@ const double initialthetarotation = 0;
 const double initialxdisplacement = 0;
 const double initialydisplacement = 0;
 const double initialzdisplacement = 0;
-const double xscalefactortweak = 0;
-const double yscalefactortweak = 0;
-const double zscalefactortweak = 0;
+const double xscalefactortweak = 1;
+const double yscalefactortweak = 1;
+const double zscalefactortweak = 1;
 
 // OPTION - what system params do you want . Don't touch these usually
 //System size parameters
